0c4083602cf7c4ba33078998c6bb9759e2f0b07d
alvindaiyan@gmail.com <alvindaiyan@gmail.com@8da8dd90-d797-c516-e8fd-8b5f68d657ae>
2013-10-05 21:54:26 +0000

    finished configure table 1 for hibernate

--- trunk/ accountingsoftware4zho/AccSoftare/src/main/java/com/accsoftware/common/App.java
+++ trunk/ accountingsoftware4zho/AccSoftare/src/main/java/com/accsoftware/common/App.java
@@ -1,13 +1,28 @@
 package com.accsoftware.common;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Date;
+
+import org.hibernate.Session;
+
+import com.accsoftware.persistence.HibernateUtil;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	 System.out.println("Maven + Hibernate + MySQL");
+         Session session = HibernateUtil.getSessionFactory().openSession();
+  
+         session.beginTransaction();
+         Table1 table1 = new Table1();
+         table1.setDate(new Date(2013, 8, 3));
+         table1.setItem("stationery");
+         table1.setFee(26.79);
+         table1.setGst(3.59);
+         table1.setStationary(23.3);
+         
+  
+         session.save(table1);
+         session.getTransaction().commit();
     }
 }
