f6239d6aca680c1bf6fddea14e7224d652bc5df4
alvindaiyan@gmail.com <alvindaiyan@gmail.com@8da8dd90-d797-c516-e8fd-8b5f68d657ae>
2013-10-06 10:02:36 +0000

    finish the ui for add function for Table1

---
+++  accountingsoftware4zho/AccSoftare/src/main/java/com/accsoftware/util/DataHandler.java
@@ -0,0 +1,26 @@
+package com.accsoftware.util;
+
+
+import org.hibernate.Session;
+
+import com.accsoftware.common.Table1;
+import com.accsoftware.persistence.HibernateUtil;
+
+public class DataHandler {
+	
+	public static void addRecord(Table1 record){
+		if(validValue(record)) throw new IllegalArgumentException("You cannot type Have Empty Value"); 
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		  
+        session.beginTransaction();		
+        session.save(record);
+        session.getTransaction().commit();
+	}
+	
+	private static boolean validValue(Table1 record){	
+		
+		return !(record.getItem() != null && record.getDate() != null
+				&& record.getFee() != null && record.getGst() != null);
+	}
+}
